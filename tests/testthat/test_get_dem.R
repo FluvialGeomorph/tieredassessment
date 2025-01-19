9213bdcae76a5f63bb48fd4856b51e976c883a51
Michael <mike.p.doc.71@gmail.com>
2025-01-19 17:33:52 -0600

    quieted test

--- tests/testthat/test_get_dem.R
+++ tests/testthat/test_get_dem.R
@@ -1,6 +1,6 @@
 test_that("dem is a terra::SpatRaster", {
   xs <- sf::st_read(system.file("extdata", "xs.geojson", 
-                              package = "tieredassessment"))
+                                package = "tieredassessment"), quiet = TRUE)
   dem <- get_dem(xs)
   #terra::plot(dem)
   expect_true("SpatRaster" %in% class(dem))
