b442a447f034d2baa4dc3755d4d49d0010a2e8a5
Michael <mike.p.doc.71@gmail.com>
2025-01-19 09:45:22 -0600

    added mapedit minimal example

--- R/app_server.R
+++ R/app_server.R
@@ -1,26 +1,34 @@
 #' The application server-side
 #'
 #' @param input,output,session Internal parameters for {shiny}.
-#'     DO NOT REMOVE.
+#'
 #' @import shiny
+#' @importFrom mapedit editMod
+#' @importFrom sf write_sf
 #' @noRd
+#' 
 app_server <- function(input, output, session) {
-    map <- leaflet() %>%
+  
+  # Create the map  
+  map <- leaflet() %>%
       addTiles()
   
-    edits <- callModule(editMod,
-                     leafmap = map,
-                     id = "map")
+  edits <- callModule(editMod,
+                      leafmap = map,
+                      id = "map",
+                      editor = "leafpm")
     
-    observeEvent(input$save, {
-        
-        geom <- edits()$finished
-        
-        if (!is.null(geom)) {
-            assign('new_geom', geom, envir = .GlobalEnv)
-            sf::write_sf(geom, 'new_geom.geojson', 
-                         delete_layer = TRUE, delete_dsn = TRUE)
-        }
-        
-    })
+  observeEvent(input$save, {
+    
+    geom <- edits()$finished
+    
+    if (!is.null(geom)) {
+      
+      
+      # Save layer
+      assign('new_geom', geom, envir = .GlobalEnv)
+      sf::write_sf(geom, 'new_geom.geojson', 
+                   delete_layer = TRUE, delete_dsn = TRUE)
+    }
+  })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -3,61 +3,28 @@
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom mapedit editModUI
 #' @noRd
+#' 
 app_ui <- function(request) {
   library(shiny)
   library(leaflet)
   library(mapedit)
   library(sf)
   
-  tagList(
-    # Leave this function for adding external resources
+  tagList(# Leave this function for adding external resources
     golem_add_external_resources(),
-    
     
     # Your application UI logic
     fluidPage(
-      # Application title
       titlePanel("Minimal Shiny Leaflet Mapedit"),
       
-      # Sidebar with a ui for grabbing mapedit data
       sidebarLayout(
-        sidebarPanel(
-          actionButton('save', 'Save from Map')
-        ),
+        position = "right",
         
-        # add map
-        mainPanel(
-          editModUI("map")
-        )
+        sidebarPanel(actionButton('save', 'Save from Map')),
+        
+        mainPanel(editModUI("map"))
       )
-    )
-  )
-}
-
-
-#' Add external Resources to the Application
-#'
-#' This function is internally used to add external
-#' resources inside the Shiny application.
-#'
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
-golem_add_external_resources <- function() {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
-
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "tiered-assessment"
-    )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert()
-    
-  )
+    ))
 }
