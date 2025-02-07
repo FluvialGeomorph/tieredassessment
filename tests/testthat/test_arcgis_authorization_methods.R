7f1b68f1d5504a55341fafb60a90e0909dc96855
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2025-02-07 16:37:23 -0600

    tested oauth authentication, got auth_client to work. Full steam ahead!

--- tests/testthat/test_arcgis_authorization_methods.R
+++ tests/testthat/test_arcgis_authorization_methods.R
@@ -5,7 +5,7 @@ test_that("Does testing OAuth token auth work?", {
   url_image_server <- "https://elevation.arcgis.com/arcgis/rest/services/WorldElevation/Terrain/ImageServer"
   # Get updated key before test
   # https://usace-mvr.maps.arcgis.com/home/item.html?id=b5e3ddc1fbb444cda8d5837693e45739
-  test_key <- "3NKHt6i2urmWtqOuugvr9VG1T8lJ7v_6OLXPvNN5QXp0kSS9pjbYWXrIRUGJHAzK7WcE_JgvIC2powyKVWHUVfraxfDqlHZFys5oSXaRZn8Brx5kxeWzBmBjW2g14ePH"
+  test_key <- "3NKHt6i2urmWtqOuugvr9TrazKNUuZVJjHbGSqYJWs5wG_O80ALtit7IoCJPyyiM-ZpuPb5PL6WjJAp8eXiNkuDuoDYvuwCcmZwxyoTGi2oMYRDF2LB1xfrxLhO44ndc"
   test_token <- httr2::oauth_token(
                      test_key,
                      arcgis_host = "https://usace-mvr.maps.arcgis.com/")
@@ -49,30 +49,23 @@ test_that("Does 'auth_user' function for auth work?", {
 test_that("Does 'auth_binding' function for auth work?", {
   skip("need to confirm with esri how to configure this")
   unset_arc_token()
-  # arcgisbinding::arc.portal_connect(
-  #  url = "https://usace-mvr.maps.arcgis.com/",
-  #  user = Sys.getenv("ARCGIS_USER"),
-  #  password = Sys.getenv("ARCGIS_PASSWORD"))
   token <- arcgisutils::auth_binding()
   set_arc_token(token)
 })
 
 test_that("Does 'auth_client' function for auth work?", {
-  skip("need to confirm with esri how to configure this")
   unset_arc_token()
+  url_image_server <- "https://elevation.arcgis.com/arcgis/rest/services/WorldElevation/Terrain/ImageServer"
   perm_token <- arcgisutils::auth_client(
-  client = Sys.getenv("ARCGIS_CLIENT"),
-  secret = Sys.getenv("ARCGIS_SECRET"),
+  client = Sys.getenv("ARCGIS_CLIENTID"),
+  secret = Sys.getenv("ARCGIS_CLIENTSECRET"),
   host = Sys.getenv("ARCGIS_HOST"),
   expiration = 120)
-  set_arc_token(perm_token)
+  
+  set_arc_token(token = perm_token)
+  token <- arc_token()
+  expect_true("httr2_token" %in% class(token))
+  imgsrv  <- arc_open(url_image_server)
+  expect_true("ImageServer" %in% class(imgsrv))
 })
 
-test_that("Does 'auth_key' function for auth work?", {
-  skip("need to confirm with esri how to configure this")
-  unset_arc_token()
-  key_token <- arcgisutils::auth_key(
-    api_key = Sys.getenv("ARCGIS_API_KEY"),
-    host    = Sys.getenv("ARCGIS_HOST"))
-  set_arc_token(key_token)
-})
