b6773757f71ce9ef900359726803bf3cfa3eb9db
Michael <mike.p.doc.71@gmail.com>
2025-02-23 16:02:44 -0600

    added more test data

--- tests/testthat/test_get_terrain_map.R
+++ tests/testthat/test_get_terrain_map.R
@@ -7,3 +7,13 @@ test_that("map is a tmap", {
   #map
   expect_true("tmap" %in% class(map))
 })
+
+test_that("map is a tmap", {
+  xs <- sf::st_read(system.file("extdata", "xs.shp", 
+                                package = "tieredassessment"), quiet = TRUE)
+  dem <- get_dem(xs)
+  map <- get_terrain_map(xs, dem)
+  tmap::tmap_mode("plot")
+  map
+  expect_true("tmap" %in% class(map))
+})
