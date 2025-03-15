5b802880c5e1735991f8fd8438c3f3ee33b6cb74
Michael <mike.p.doc.71@gmail.com>
2025-03-15 17:44:40 -0500

    began creating draw_xs_module tests

--- tests/testthat/test_draw_xs_module.R
+++ tests/testthat/test_draw_xs_module.R
@@ -1,3 +1,29 @@
+test_that("outpu instructionst", {  
+  testServer(draw_xs_server, args = list(), {
+    # test outputs
+    #print(session$output$draw_xs_instructions$html)
+    expect_true(is.character(session$output$draw_xs_instructions$html))
+    
+    # test variables inside server function
+    #print(xs())
+    expect_true("sf" %in% class(xs()))
+    #print(class(draw_xs_map))
+    expect_true("leaflet" %in% class(draw_xs_map))
+    
+    #print(draw_xs())
+    
+    # test return values
+    print(session$getReturned())
+    
+    #new_xs <- draw_xs_server("1")
+    #print(new_xs)
+  })
+})
+
+test_that("", {
+  xs <- callModule(draw_xs_server, "1")
+})
+
 test_that("", {
   # create reactives
   xs <- reactiveVal()
@@ -7,6 +33,6 @@ test_that("", {
     x <- ""
     session$flushReact()
     
-    expect_equal()
+    expect_true(is.character(session$output$draw_xs_instructions$html))
   })
 })
