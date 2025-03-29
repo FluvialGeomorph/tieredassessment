d6f9401496bd43dd4611066d00935663f1fd6123
Michael <mike.p.doc.71@gmail.com>
2025-03-29 16:05:14 -0500

    updated get_dem to test for 3857 input and enforce 3857 output

--- tests/testthat/test_get_dem.R
+++ tests/testthat/test_get_dem.R
@@ -1,16 +1,23 @@
 test_that("WGS84 input - dem is a terra::SpatRaster", {
-  xs <- sf::st_read(system.file("extdata", "xs.geojson", 
+  xs_geojson <- sf::st_read(system.file("extdata", "xs.geojson", 
                                 package = "tieredassessment"), quiet = TRUE)
+  xs <- sf::st_transform(xs_geojson, crs = 3857)
   dem <- get_dem(xs)
   #terra::plot(dem)
   expect_true("SpatRaster" %in% class(dem))
 })
 
 test_that("web mercator input - dem is a terra::SpatRaster", {
-  xs <- sf::st_read(system.file("extdata", "xs.geojson", 
+  xs_shp <- sf::st_read(system.file("extdata", "xs.geojson", 
                                 package = "tieredassessment"), quiet = TRUE)
-  xs_3857 <- sf::st_transform(xs, crs = 3857)
-  dem <- get_dem(xs_3857)
+  xs <- sf::st_transform(xs_shp, crs = 3857)
+  dem <- get_dem(xs)
   #terra::plot(dem)
   expect_true("SpatRaster" %in% class(dem))
+})
+
+test_that("expect error - WGS84 input", {
+  xs <- sf::st_read(system.file("extdata", "xs.geojson", 
+                                package = "tieredassessment"), quiet = TRUE)
+  expect_error(get_dem(xs))
 })
