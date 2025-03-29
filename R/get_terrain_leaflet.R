0e7fb782be4dbaf0604ede5f7ddb5cd063ee7d83
Michael <mike.p.doc.71@gmail.com>
2025-03-29 13:09:19 -0500

    added get_terrain_leaflet function

--- R/get_terrain_leaflet.R
+++ R/get_terrain_leaflet.R
@@ -8,12 +8,12 @@
 #' @return a leaflet object
 #' @export
 #' 
-#' @importFrom leaflet colorNumeric addRasterImage addLegend addProviderTiles
-#'                     addLayersControl
+#' @importFrom leaflet colorNumeric addRasterImage 
+#' @importFrom leafem updateLayersControl customizeLayersControl
 #' @importFrom leaflegend addLegendNumeric
 #' @importFrom fluvgeo map_extent
 #' @importFrom assertthat assert_that
-#' @importFrom sf st_as_sf st_as_sfc
+#' @importFrom sf st_as_sf st_as_sfc st_bbox
 #' @importFrom terra crop minmax values
 #'
 get_terrain_leaflet <- function(xs, dem) {
@@ -21,15 +21,16 @@ get_terrain_leaflet <- function(xs, dem) {
                msg = "must be sf object")
   assert_that("SpatRaster" %in% class(dem), 
               msg = "must be SpatRaster object")
+  
   # set extent
   xs_extent <- fluvgeo::map_extent(xs, extent_factor = 1.2)
   xs_extent_poly <- sf::st_as_sf(sf::st_as_sfc(xs_extent))
   
   # Crop the dem to the cross section map extent (+ 20 pixels)
-  dem_i <- terra::crop(x = dem,
-                       y = terra::ext(xs_extent_poly) + 20)
+  crop_extent <- terra::ext(xs_extent_poly) + 20
+  dem_i <- terra::crop(x = dem, y = crop_extent)
   
-  # Create a topo color mapping
+  # Create a leaflet topo color mapping
   leaflet_topo <- colorNumeric(
     palette = c("cadetblue2", "khaki1", "chartreuse4", "goldenrod1", 
                 "orangered4", "saddlebrown", "gray70", "white"),
@@ -37,23 +38,35 @@ get_terrain_leaflet <- function(xs, dem) {
     na.color = "transparent",
     reverse = FALSE)
   
-  terrain_leaflet <- leaflet() %>%
-    addRasterImage(dem_i,
-                   colors = leaflet_topo,
-                   group = "Elevation",
-                   opacity = 0.7, project = FALSE) %>%
-    addLegendNumeric(pal = leaflet_topo,
-                     values = values(dem_i),
-                     title = "NAVD88, ft",
-                     decreasing = TRUE,
-                     group = "Elevation",
-                     position = "topright") %>%
-    addProviderTiles("USGS.USTopo", group = "USGS Topo") %>%
-    addProviderTiles("Esri.WorldImagery", group = "Imagery") %>%
-    addLayersControl(
-      baseGroups = c("USGS Topo", "Imagery"),
-      overlayGroups = c("Elevation"),
-      position = "topleft")
+  dem_bbox <- sf::st_bbox(crop_extent)
+  
+  terrain_leaflet <- 
+    get_leaflet(
+      search = FALSE) %>%
+    addRasterImage(
+      dem_i,
+      colors = leaflet_topo,
+      group = "Elevation",
+      opacity = 0.7, project = FALSE) %>%
+    addLegendNumeric(
+      pal = leaflet_topo,
+      values = values(dem_i),
+      title = "NAVD88, ft",
+      decreasing = TRUE,
+      group = "Elevation",
+      position = "topright") %>%
+    updateLayersControl(
+      addOverlayGroups = c("Elevation"),
+      position = "topleft") %>%
+    # customizeLayersControl(
+    #   view_settings = list(
+    #     "Elevation" = list(
+    #       coords = as.numeric(c(dem_bbox$ymin, dem_bbox$xmin, 
+    #                           dem_bbox$ymax, dem_bbox$xmax)),
+    #       #options = list(padding = c(2, 2)),
+    #       fly = TRUE)
+    #     ),
+    #   home_btns = TRUE)
   
   #terrain_leaflet
   return(terrain_leaflet)
