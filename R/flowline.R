a1c27f6b12afc790326068d4c3c14ebfe07b436b
Michael <mike.p.doc.71@gmail.com>
2025-04-01 17:11:49 -0500

    updated flowline function

--- R/flowline.R
+++ R/flowline.R
@@ -1,3 +1,28 @@
-flowline <- function(dem) {
+#' @title Flowline
+#' 
+#' @description Takes a newly drawn flowline and uses the dem to ensure
+#' the flowline is digitized in the upstream direction.  
+#' @param flowline 
+#' @param dem 
+#'
+#' @returns a valid flowline
+#' @export
+#' 
+#' @importFrom fluvgeo sf_line_end_point
+#' 
+flowline <- function(flowline, dem) {
+  # Determine downstream end of flowline
+  og_start_pt <- flowline %>%
+    sf_line_end_point("start")
+    
+  og_start_pt_z <- sf_get_z(og_start_pt, dem)
   
+  
+  og_end_pt   <- sf_line_end_point(flowline, "end")
+  
+  # Set flowline direction
+  
+  # Create route
+  
+  return(fl)
 }
--- tests/testthat/test_flowline.R
+++ tests/testthat/test_flowline.R
@@ -1,6 +1,10 @@
 test_that("map is a tmap", {
-  xs <- sf::st_read(system.file("extdata", "xs.shp", 
+  xs_shp <- sf::st_read(system.file("extdata", "xs.shp", 
+                                    package = "tieredassessment"), quiet = TRUE)
+  xs <- sf::st_transform(xs_shp, crs = 3857) # Web Mercator
+  fl_shp <- sf::st_read(system.file("extdata", "fl.shp", 
                                 package = "tieredassessment"), quiet = TRUE)
+  flowline <- sf::st_transform(fl_shp, crs = 3857) # Web Mercator
   dem <- get_dem(xs)
   
 })
