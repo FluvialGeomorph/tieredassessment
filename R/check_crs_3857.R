a67291a1249bb0c6f7afb0b5dc856aa1cfbe2591
Michael <mike.p.doc.71@gmail.com>
2025-04-02 17:11:51 -0500

    updated check_crs_3857 function

--- R/check_crs_3857.R
+++ R/check_crs_3857.R
@@ -8,11 +8,16 @@
 check_crs_3857 <- function(x) {
   # Define the reference crs
   reference_crs <- data.frame() %>%
-    st_as_sf(geometry = st_sfc(), crs = "EPSG:3857")  # ensure Web Mercator
+    st_as_sf(geometry = st_sfc(), crs = 3857)  # ensure Web Mercator
   
   assert_that(st_crs(reference_crs) == st_crs(x), 
               msg = "the input CRS must be EPSG:3857")
   
+  # add section to test bbox
+  
+  print(paste("EPSG: ", as.character(st_crs(x)$epsg)))
+  print("bbox:")
+  print(st_bbox(x))
   # Return TRUE if all assertions are met
   TRUE
 }
