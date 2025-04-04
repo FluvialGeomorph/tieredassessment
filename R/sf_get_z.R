7fb915141ba05deb547f349a394e7c2fc4d95829
Michael <mike.p.doc.71@gmail.com>
2025-04-03 22:09:56 -0500

    updated sf_get_z function

--- R/sf_get_z.R
+++ R/sf_get_z.R
@@ -1,18 +1,20 @@
 #' @title SF Get Z
 #' @description Adds a new attribute "Z" to the input points sf object based
 #' on the input dem.
-#' @param points
-#' @param dem
+#' @param points sf object;
+#' @param dem    terra SpatRast object;
 #'
-#' @returns
+#' @returns point sf object with new field containing dem z
 #' @export
 #'
 #' @importFrom fluvgeo sf_point_attributes
 #' @importFrom terra vect extract
 #' 
 sf_get_z <- function(points, dem) {
-  pts <- terra::vect(points)
-  
-  pts_z <- terra::extract(x = pts, y = dem)
+  assert_that(st_crs(points) == st_crs(dem), 
+              msg = "points and dem must be have the same crs")
+
+  pts_z <- extract(x = dem, y = vect(points), 
+                   xy = TRUE, layer = 1)
   return(pts_z)
 }
