72537293bfd5cbc928b0755f1a5ed4f30b2ea1cb
Michael <mike.p.doc.71@gmail.com>
2025-04-03 22:08:42 -0500

    updated flowline function

--- R/flowline.R
+++ R/flowline.R
@@ -2,27 +2,51 @@
 #' 
 #' @description Takes a newly drawn flowline and uses the dem to ensure
 #' the flowline is digitized in the upstream direction.  
-#' @param flowline 
-#' @param dem 
+#' @param flowline sf object;
+#' @param dem      terra SpatRast object;
 #'
-#' @returns a valid flowline
+#' @returns a valid flowline sf object
 #' @export
 #' 
-#' @importFrom fluvgeo sf_line_end_point
+#' @importFrom assertthat assert_that
+#' @importFrom sf st_crs st_within
+#' @importFrom dplyr mutate select arrange
+#' @importFrom fluvgeo sf_line_end_point sf_line_reverse
+
 #' 
 flowline <- function(flowline, dem) {
+  flowline <- sf_fix_crs(flowline)
+  assert_that(st_crs(flowline) == st_crs(dem), 
+              msg = "flowline and dem must be have the same crs")
+  assert_that(st_within(flowline, 
+                        st_sf(st_as_sfc(st_bbox(dem))), sparse = FALSE),
+              msg = "flowline must be within the dem")
+  
   # Determine downstream end of flowline
   og_start_pt <- flowline %>%
-    sf_line_end_point("start")
-    
-  og_start_pt_z <- sf_get_z(og_start_pt, dem)
+    sf_line_end_point("start") %>%
+    sf_get_z(., dem) %>%
+    mutate(z = value) %>%
+    select(c(x, y, z))
   
+  og_end_pt <- flowline %>%
+    sf_line_end_point("end") %>%
+    sf_get_z(., dem) %>%
+    mutate(z = value) %>%
+    select(c(x, y, z))
   
-  og_end_pt   <- sf_line_end_point(flowline, "end")
-  
-  # Set flowline direction
-  
-  # Create route
-  
+  # Correct flowline digitization direction if needed
+  if(og_end_pt$z < og_start_pt$z) {
+    print("reverse the flowline")
+    fl <- sf_line_reverse(flowline)
+  } else {
+    fl <- flowline
+  }
+ 
+  #plot(dem)
+  #lines(vect(fl), col = "blue")
+  #points(vect(og_start_pt, geom = c("x", "y"), crs = 3857), col = "green")
+  #points(vect(og_end_pt, geom = c("x", "y"), crs = 3857), col = "red")
+
   return(fl)
 }
--- tests/testthat/test_flowline.R
+++ tests/testthat/test_flowline.R
@@ -1,10 +1,9 @@
 test_that("map is a tmap", {
-  xs_shp <- sf::st_read(system.file("extdata", "xs.shp", 
-                                    package = "tieredassessment"), quiet = TRUE)
-  xs <- sf::st_transform(xs_shp, crs = 3857) # Web Mercator
   fl_shp <- sf::st_read(system.file("extdata", "fl.shp", 
                                 package = "tieredassessment"), quiet = TRUE)
-  flowline <- sf::st_transform(fl_shp, crs = 3857) # Web Mercator
-  dem <- get_dem(xs)
-  
+  fl_fix <- sf_fix_crs(fl_shp)
+  flowline <- sf::st_transform(fl_fix, crs = 3857) # Web Mercator
+  dem <- get_dem(flowline)
+  fl <- flowline(flowline, dem)
+  expect_true("sf" %in% class(fl))
 })
