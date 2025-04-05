d5d0c901fcfda56bf4a62e861bc68324feb6ecd6
Michael <mike.p.doc.71@gmail.com>
2025-04-05 16:18:21 -0500

    updated test_flowline with more rigorous tests

--- R/flowline.R
+++ R/flowline.R
@@ -24,31 +24,25 @@ flowline <- function(flowline, dem) {
   #                       st_sf(st_as_sfc(st_bbox(dem))), sparse = FALSE),
   #             msg = "flowline must be within the dem")
   
+  
   # Determine downstream end of flowline
   og_start_pt <- flowline %>%
     sf_line_end_point("start") %>%
     sf_get_z(., dem) %>%
-    mutate(z = value) %>%
     select(c(x, y, z))
   
   og_end_pt <- flowline %>%
     sf_line_end_point("end") %>%
     sf_get_z(., dem) %>%
-    mutate(z = value) %>%
     select(c(x, y, z))
   
-  # Correct flowline digitization direction if needed
+  # Reverse the flowline digitization direction if needed
   if(og_end_pt$z < og_start_pt$z) {
     print("reverse the flowline")
     fl <- sf_line_reverse(flowline)
   } else {
     fl <- flowline
   }
- 
-  #plot(dem)
-  #lines(vect(fl), col = "blue")
-  #points(vect(og_start_pt, geom = c("x", "y"), crs = 3857), col = "green")
-  #points(vect(og_end_pt, geom = c("x", "y"), crs = 3857), col = "red")
 
   return(fl)
 }
