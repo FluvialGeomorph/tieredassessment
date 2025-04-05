40e2157cfa5e6e04dba6214c9e8ef93c2fe0a896
Michael <mike.p.doc.71@gmail.com>
2025-04-05 16:19:49 -0500

    updated tests to use new test data

--- tests/testthat/test_check_crs_3857.R
+++ tests/testthat/test_check_crs_3857.R
@@ -1,12 +1,12 @@
 test_that("input is not 3857", {
-  xs_geojson <- sf::st_read(system.file("extdata", "xs.geojson", 
+  xs_mapedit <- sf::st_read(system.file("extdata", "xs_mapedit.shp", 
                                 package = "tieredassessment"), quiet = TRUE)
-  expect_error(check_crs_3857(xs_geojson))
+  expect_error(check_crs_3857(xs_mapedit))
 })
 
 test_that("input is 3857", {
-  xs_geojson <- sf::st_read(system.file("extdata", "xs.geojson", 
-                                        package = "tieredassessment"), quiet = TRUE)
-  xs <- sf::st_transform(xs_geojson, crs = "EPSG:3857") # Web Mercator
+  xs_mapedit <- sf::st_read(system.file("extdata", "xs_mapedit.shp", 
+                                package = "tieredassessment"), quiet = TRUE)
+  xs <- sf::st_transform(xs_mapedit, crs = 3857) # Web Mercator
   expect_true(check_crs_3857(xs))
 })
--- tests/testthat/test_get_dem.R
+++ tests/testthat/test_get_dem.R
@@ -1,34 +1,24 @@
 test_that("WGS84 input - dem is a terra::SpatRaster", {
-  xs_geojson <- sf::st_read(system.file("extdata", "xs.geojson", 
+  xs_mapedit <- sf::st_read(system.file("extdata", "xs_mapedit.shp", 
                                 package = "tieredassessment"), quiet = TRUE)
-  xs <- sf::st_transform(xs_geojson, crs = 3857)
+  xs <- sf::st_transform(xs_mapedit, crs = 3857) # Web Mercator
   dem <- get_dem(xs)
   #terra::plot(dem)
   expect_true("SpatRaster" %in% class(dem))
 })
 
 test_that("web mercator input - dem is a terra::SpatRaster", {
-  xs_shp <- sf::st_read(system.file("extdata", "xs.geojson", 
-                                package = "tieredassessment"), quiet = TRUE)
-  xs <- sf::st_transform(xs_shp, crs = 3857)
+  xs <- sf::st_read(system.file("extdata", "xs.shp", 
+                       package = "tieredassessment"), quiet = TRUE)
   dem <- get_dem(xs)
   #terra::plot(dem)
   expect_true("SpatRaster" %in% class(dem))
 })
 
 test_that("expect error - WGS84 input", {
-  xs <- sf::st_read(system.file("extdata", "xs.geojson", 
+  xs_mapedit <- sf::st_read(system.file("extdata", "xs_mapedit.shp", 
                                 package = "tieredassessment"), quiet = TRUE)
-  expect_error(get_dem(xs))
-})
-
-test_that("leaflet from shapefile", {
-  xs_mapedit_shp <- sf::st_read(system.file("extdata", "xs_mapedit.shp", 
-                                    package = "tieredassessment"), quiet = TRUE)
-  #plot(xs_mapedit_shp)
-  xs <- sf::st_transform(xs_mapedit_shp, crs = 3857) # Web Mercator
-  st_crs(xs_mapedit_shp) == st_crs(xs)
-  dem <- get_dem(xs)
+  dem <- get_dem(xs_mapedit)
   #terra::plot(dem)
-  expect_true("SpatRaster" %in% class(dem))
+  expect_error()
 })
