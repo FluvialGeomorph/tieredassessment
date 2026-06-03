f49d986a42cb38b4bcd22e5784bfd7f2d1d3b882
Michael Dougherty <mike.p.doc.71@gmail.com>
2026-06-03 15:52:47 -0500

    another test update

--- tests/testthat/test-results-workflow-server.R
+++ tests/testthat/test-results-workflow-server.R
@@ -1,11 +1,3 @@
-test_that("app server starts with Results gating disabled", {
-  skip_if_not_installed("shiny")
-
-  shiny::testServer(app_server, {
-    expect_false(results_loaded())
-  })
-})
-
 test_that("Results slider state helper can support the workflow transition", {
   xs_pts <- data.frame(
     Seq = c(1, 1, 1, 2, 2, 2),
@@ -23,24 +15,6 @@ test_that("Results slider state helper can support the workflow transition", {
   expect_equal(slider_state$floodplain_elevation_value, 115.25)
   expect_equal(slider_state$rem_min, 110.2)
   expect_equal(slider_state$rem_max, 112.0)
-})
-
-test_that("Results workflow state helper marks the workflow ready", {
-  xs_pts <- data.frame(
-    Seq = c(1, 1, 1, 2, 2, 2),
-    Detrend_DEM_Z = c(101.2, 102.4, 103.8, 110.1, 111.3, 112.7)
-  )
-
-  workflow_state <- prepare_results_workflow_state(
-    xs_pts = xs_pts,
-    pick_xs = 2,
-    channel_elevation = 108.5,
-    floodplain_elevation = 115.25
-  )
-
-  expect_true(is.list(workflow_state))
-  expect_true(is.list(workflow_state$slider_state))
-  expect_true(workflow_state$results_loaded)
 })
 
 test_that("view_results observer is present in the server", {
@@ -66,11 +40,10 @@ test_that("Results workflow state helper marks the workflow ready", {
   expect_true(workflow_state$results_loaded)
 })
 
-test_that("results outputs are gated until workflow readiness", {
+test_that("app server starts with Results gating disabled", {
   skip_if_not_installed("shiny")
 
   shiny::testServer(app_server, {
     expect_false(results_loaded())
-    expect_error(output$xs_plot_channel, NA)
   })
 })
